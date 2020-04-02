dded6ae8010d16c84a9a79e7abe2908a35aee1d6
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-01 21:50:34 -0400

    chore: App architecture setup

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class App extends Component {
+
+  componentDidMount() {
+    // initialize the data here
+  }
+
+  render() {
+    return (
+      <div>
+        Would you rather
+      </div>
+    )
+  }
+}
+
+
+function mapStateToProps() {
+  // need to grap the user here (passed in the arguments)
+  // and add a loading property to the state
+
+  return {}
+}
+
+export default App
