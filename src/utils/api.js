cf23a119d20d9c7462810de49a838f2b75e69f48
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-01 22:26:25 -0400

    feat: Add api utility functions

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer
+} from './_DATA'
+
+export async function getInitialData () {
+  try {
+    const users = await _getUsers()
+    const questions = await _getQuestions()
+    return {
+      users,
+      questions
+    }
+  } catch (e) {
+    console.error(e);
+    return {}
+  }
+
+}
