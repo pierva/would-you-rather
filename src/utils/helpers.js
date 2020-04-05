eb13282511d21851781fa603c5851bc4380d5016
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-05 15:08:39 -0400

    feat: Helper function to format question

---
+++ src/utils/helpers.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @param {Object} question Question object from data api
+ * @param {Object} author Object containing all the author information
+ * @param {string} authedUser ID of the authenticated user
+ */
+
+export function formatQuestion(question, author, authedUser) {
+  const { id, optionOne, optionTwo, timestamp, } = question
+  const { name, avatarURL } = author 
+
+  let hasAnswered = {}
+  if (optionOne.votes.includes(authedUser)) {
+    hasAnswered.value = true
+    hasAnswered.option = 'optionOne'
+  } else if (optionTwo.votes.includes(authedUser)){
+    hasAnswered.value = true
+    hasAnswered.option = 'optionTwo'
+  }
+
+  return {
+    id,
+    timestamp,
+    name,
+    avatar: avatarURL,
+    hasAnswered,
+    optionOne,
+    optionTwo
+  }
+}
