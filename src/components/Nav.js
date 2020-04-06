bb0e5c7654fd954bc6eb91e8c77c58c23cd7a026
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-06 15:20:57 -0400

    refactor: Navigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,73 @@
 import React from 'react'
-import { NavLink, Redirect } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Nav(props) {
+  const authLinks = [
+    {
+      path: '/',
+      text: 'Home',
+      options: {}
+    },
+    {
+      path: '/new',
+      text: 'New Question',
+      options: {}
+    },
+    {
+      path: '/leaderboard',
+      text: 'Leaderboard',
+      options: {}
+    },
+    {
+      path: '/',
+      text: `Logged as ${props.authedUser} | LOGOUT`,
+      options: 
+        {
+          clickHandler: props.handleLogout
+        }
+    }
+  ]
+
+  const unauthLinks = [
+    {
+      path: '/',
+      text: 'Home',
+      options: {}
+    },
+    {
+      path: '/leaderboard',
+      text: 'Leaderboard',
+      options: {}
+    },
+    {
+      path: '/login',
+      text: 'LOGIN',
+      options: {}
+    }
+  ]
+
   return (
     <nav>
       <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' exact activeClassName='active'>
-            New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leader Board
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/' exact onClick={props.handleLogout}>
-            Logged as {props.authedUser} | LOGOUT
-          </NavLink>
-        </li>
+        {props.authedUser ? 
+        authLinks.map((link) => (
+          <li key={link.text}>
+            <NavLink to={link.path} exact activeClassName='active'
+              onClick={link.options.clickHandler ? link.options.clickHandler : null}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+          ))
+        : 
+        unauthLinks.map((link) => (
+          <li key={link.text}>
+            <NavLink to={link.path} exact activeClassName='active'>
+              {link.text}
+            </NavLink>
+          </li>
+          ))
+        }
       </ul>
     </nav>
   )
