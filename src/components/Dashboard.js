7fa3dfa5b174e586cf81b2e42afcf69dca4760fe
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-07 11:39:55 -0400

    fix: Show answered questions by user

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,11 +8,27 @@ class Dashboard extends Component {
     answered: true
   }
 
+  toggleGroup = () => {
+    this.setState((prevState) => ({
+      answered: !prevState.answered
+    }))
+  }
+
   render() {
-    console.log(this.props);
-    
+    // Show all the questions if there is no logged user
+    if(!this.props.authedUser) {  
+      return (
+        <div>
+          <h2>Please login to check your status</h2>
+          <QuestionsGroup questionIds={this.props.allIds} />
+        </div>
+      )
+    }
     return (
       <div>
+        <button onClick={this.toggleGroup}>
+          {this.state.answered ? "Unanswered" : "Answered"}
+        </button>
         {this.state.answered 
           ? <QuestionsGroup questionIds={this.props.answeredIds} />
           : <QuestionsGroup questionIds={this.props.notAnsweredIds} />
@@ -22,20 +38,27 @@ class Dashboard extends Component {
   }
 }
 
-function mapStateToProps ({ questions }) {
+function mapStateToProps ({ questions, authedUser }) {
   const answeredIds = []
   const notAnsweredIds = []
+  const allIds = []
   
   Object.keys(questions) 
       .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
       .forEach((key) => {
+        // loop over the questions and check if logged user has answered
+        // the question
         const wasAnswered = questions[key].optionOne.votes
-          .concat(questions[key].optionTwo.votes).length > 0 ? true : false
+          .concat(questions[key].optionTwo.votes)
+          .includes(authedUser) ? true : false
         wasAnswered ? answeredIds.push(key) : notAnsweredIds.push(key)
+        allIds.push(key)
       })  
   return {
     answeredIds,
-    notAnsweredIds
+    notAnsweredIds,
+    allIds,
+    authedUser
   }
 }
 
