41627d22dc7601e7869bbbb4c29919407aba34bd
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-10 13:48:07 -0400

    feat: Helper function to format users by score

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31,3 +31,45 @@ export function formatQuestion(question, author, authedUser) {
     optionTwo
   }
 }
+/**
+ * 
+ * @param {Object} users Must have the following properties: 
+ *                       id, questions, answers
+ * @param {Object} users[key]
+ * @param {string} users[key].id user id
+ * @param {Object} users[key].answers
+ * @param {Array} users[key].question
+ * 
+ * @returns {Object} Object with following properties:
+ *                   score: int
+ *                   questions: int
+ *                   answers: int
+ *                   userId: string
+ */
+export function sortByScore (users) {
+  const sorted = []
+  const scores = {}
+  Object.keys(users).sort((a,b) => {
+    const userAQ = users[a].questions.length
+    const userAA = Object.keys(users[a].answers).length
+    const userBQ = users[b].questions.length
+    const userBA = Object.keys(users[b].answers).length
+    scores[users[a].id] = {
+      score: userAQ + userAA ,
+      questions: userAQ,
+      answers: userAA,
+      userId: users[a].id
+    }
+    scores[users[b].id] = {
+      score: userBQ + userBA,
+      questions: userBQ,
+      answers: userBA,
+      userId: users[b].id
+    }
+    return (userBQ + userBA) - (userAQ + userAA )
+  })
+  .forEach((key) => {
+    sorted.push(scores[key])
+  })
+  return sorted
+}
