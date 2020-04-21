0ba79fcb4eb75e31f7cd0cebbb160e756179d70d
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-04-21 00:01:38 -0400

    style: Poll result

--- src/components/PollResult.js
+++ src/components/PollResult.js
@@ -1,24 +1,49 @@
 import React from 'react'
 
-export default function PollResult (props) {
+export default function PollResult(props) {
   const { question } = props
   const totVotes = question.optionOne.votes.length + question.optionTwo.votes.length
+  const optionOneVotes = question.optionOne.votes.length
+  const optionTwoVotes = question.optionTwo.votes.length
+  const scoreOne = Math.round(optionOneVotes / totVotes * 100)
+  const scoreTwo = Math.round(optionTwoVotes / totVotes * 100)
   return (
     <div>
-      <div>
-        {/* Show user selected answer if present*/}
-        {question.hasAnswered.option === "optionOne" && 
-          <span>Your selection</span>}
-        <h4>{question.optionOne.text}</h4>
-        <p>votes: {question.optionOne.votes.length} out of {totVotes}</p>
-      </div>
-      <hr />
-      <div>
-        {/* Show user selected answer if present*/}
-        {question.hasAnswered.option === "optionTwo" && 
-          <span>Your selection</span>}
-        <h4>{question.optionTwo.text}</h4>
-        <p>votes: {question.optionTwo.votes.length} out of {totVotes}</p>
+      <h2 className="card-header">
+        Would you rather:
+      </h2>
+      <div className="question-group">
+        <img className="avatar-big" src={question.avatar} />
+        <div className="inner-group">
+        <div className={question.hasAnswered.option === "optionOne" 
+                ? 'option-selected' : ''}>
+            <p className="poll-option">{question.optionOne.text}</p>
+            <div className="score-bar">
+              <div className="filler text-center"
+                style={{ width: `${scoreOne}%` }}>
+                {scoreOne}%
+              </div>
+            </div>
+            <h6 className="text-center">
+              {optionOneVotes} out of {totVotes}
+            </h6>
+          </div>
+
+          <div className={question.hasAnswered.option === "optionTwo" 
+                ? 'option-selected' : ''}>
+              
+            <p className="poll-option">{question.optionTwo.text}</p>
+            <div className="score-bar">
+              <div className="filler text-center"
+                style={{ width: `${scoreTwo}%` }}>
+                {scoreTwo}%
+              </div>
+            </div>
+            <h6 className="text-center">
+              {optionTwoVotes} out of {totVotes}
+            </h6>
+          </div>
+        </div>
       </div>
     </div>
   )
